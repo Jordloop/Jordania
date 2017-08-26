2bcd3fd3dbde707ffd7ee56e564a78dbc0b62f6e
JordLoop <Jordloop@gmail.com>
2017-08-26 00:10:07 -0700

    add displaying Repos Objects in DOM

--- src/Jordania/Models/Repos.cs
+++ src/Jordania/Models/Repos.cs
@@ -14,7 +14,8 @@ namespace Jordania.Models
         public string name { get; set; }
         public string html_url { get; set; }
         public string stargazers_count { get; set; }
-
+        public string language { get; set; }
+        public string description { get; set; }
 
         public static List<Repos> MyStarredRepos()
         {
